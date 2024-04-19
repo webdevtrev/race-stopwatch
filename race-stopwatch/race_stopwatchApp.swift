a10dd4375db0798868fb4208deba1eb87dd53993
webdevtrev <66264441+webdevtrev@users.noreply.github.com>
2024-04-19 19:04:56 -0400

    Initial Commit

---
+++ race-stopwatch/race_stopwatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  race_stopwatchApp.swift
+//  race-stopwatch
+//
+//  Created by Trevor Cash on 4/19/24.
+//
+
+import SwiftUI
+
+@main
+struct race_stopwatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
